4efb6d9953e8235e7f270c02c6d02a3edb1c85e9
DIEU Anthony (SNCF VOYAGEURS / TECHNICENTRE DE VENISSIEUX / Equ Maintenance) <8402956y@commun.ad.sncf.fr>
2023-02-28 13:41:27 +0100

    first commit

---
+++ Classroom.java
@@ -0,0 +1,11 @@
+class Classroom {
+ public static void main(String[] args) {
+
+  Wilder jack = new Wilder("Jack", true);
+  Wilder david = new Wilder("David", false);
+  
+ 
+ System.out.println(jack.whoAmI());
+ System.out.println(david.whoAmI());
+ }
+}
